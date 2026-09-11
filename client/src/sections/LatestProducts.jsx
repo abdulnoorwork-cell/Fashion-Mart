2d035411b18d7941b281a2fd10d55b0a1c8c41fb
abdulnoorwork-cell <abdulnoorwork@gmail.com>
2026-09-11 09:35:16 +0500

    save

--- client/src/sections/LatestProducts.jsx
+++ client/src/sections/LatestProducts.jsx
@@ -34,7 +34,7 @@ const LatestProducts = () => {
           <motion.div
             className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 sm:gap-4"
           >
-            {latestProducts.map((product) => (
+            {latestProducts.map((product,index) => (
               <motion.div
                 key={product.id}
                 initial={{
